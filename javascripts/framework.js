53a50328b5ebd38b6d1d0cd272ba7336f562b036
Tyler Guillen <tyguillen@gmail.com>
2016-05-06 17:43:02 -0400

    add change callbacks to model

--- javascripts/framework.js
+++ javascripts/framework.js
@@ -1,22 +1,45 @@
 var ModelConstructor = function(options) {
   var current_id = 0;
 
-  var Model = function() {
+  var Model = function(options) {
     current_id++;
+    var self = this;
 
-    this.attributes = {};
-    this.id = current_id;
-    this.attributes.id = current_id;
+    self.attributes = options || {};
+
+    if (self.attributes.change && _.isFunction(self.attributes)) {
+      self.__events.push(self.attributes.change);
+    }
+
+    self.id = current_id;
+    self.attributes.id = current_id;
   };
 
   Model.prototype = {
+    __events: [],
+
+    triggerChange: function() {
+      this.__events.forEach(function(event) {
+        event();
+      });
+    },
+
+    addCallback: function(callback) {
+      if (_.isFunction(callback)) { this.__events.push(callback); }
+    },
+
     set: function(prop, new_value) {
       this.attributes[prop] = new_value;
-      if (this.change) { this.change(); }
+      this.triggerChange();
     },
 
     get: function(prop) {
       return this.attributes[prop];
+    },
+
+    remove: function(prop) {
+      delete this.attributes[prop];
+      this.triggerChange();
     }
   };
 
